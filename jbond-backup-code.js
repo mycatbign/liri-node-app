9e1627013b9524262c2ebb9011e25fe5b6e1eb33
Jeff Bond <jbond2663@yahoo.com>
2018-11-28 13:23:15 -0500

    update with movie, concert, do what it says versions working

--- jbond-backup-code.js
+++ jbond-backup-code.js
@@ -31,3 +31,81 @@ inquirer
         // run the liri
         //===========================================================================
     }); //inquirer .then command
+
+
+
+        //===========================================================================
+        // sample spotify command
+        //===========================================================================
+
+    .then(function (inquirerResponse) {
+    userCommand = inquirerResponse.choice;
+
+    switch (userCommand) {
+        case 'spotify-this-song':
+            inquirer.prompt([{
+                type: "input",
+                message: "what song?",
+                name: "search"
+            },
+            ])
+                .then(function (inquirerResponse) {
+                    userInput = inquirerResponse.search;
+                    const keys = require("./keys");
+                    keys.spotify
+                        .search({
+                            type: 'tracl',
+                            query: '${userInput}',
+                            limit: '1'
+                        })
+                        .then(function (response) {
+                            console.log('artists name: ${response.tracks.items[0].artist...)
+        })
+                        .catch(function (err) {
+                            console.log(err);
+                            console.log("error")
+                        });
+                })
+            break;
+            //end spotify commmand
+
+
+            //===========================================================================
+            // sample spotify search
+            //===========================================================================
+            function venueSearch(input)) {
+    var artist = input;
+    artist = artist.charAt(0).toUpperCase() + artist.slice(1);
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            console.log("Here are " + response.data.length + " upcoming concert venues for " + artist + ".\r\n");
+            for i = 0, i < response.data.length; i++) {
+                var venue = response.data[i].venue;
+                var date = response.data[i].datetime;
+                var formatDate = moment(date).format("MM/DD/YYYY");
+                var dateUntil = moment(date).diff(moment(), 'days');
+                console.log("this" + artist + "concert is at" + venue.name + ".");
+                console.log("The " + artist + " concert is located in " + venue.city + "," + venue.region + ", " + venue.country + ". ");
+                console.log("The " + artisit + "conscert is on" + formatDate + ".");
+            }
+        }
+    );
+}
+
+//===========================================================================
+// define valid use cases
+//===========================================================================
+//  node liri.js
+//  node liri.js concert-this 
+//  node liri.js concert-this "Imagine Dragons"
+//  node liri.js spotify-this-song 
+//  node liri.js spotify-this-song Radioactive
+//  node liri.js movie-this 
+//  node liri.js movie-this Frozen
+//  node liri.js do-what-it-says 
+
+//===========================================================================
+// error check and make sure we have two arguments
+//===========================================================================
+
+
--- liri.js
+++ liri.js
@@ -1,147 +1,206 @@
 //===========================================================================
-// set up work
-//===========================================================================
-
 // 
+//===========================================================================
 require("dotenv").config();
 
-// we will use axios packages to prepare data for queries
-var axios = required("axios");
+//===========================================================================
+// initiate axios for packaging with the spotify API
+//===========================================================================
+var axios = require("axios");
 
+//===========================================================================
+// initiate inquirer to get input from the user from the command line
+//===========================================================================
+var inquirer = require("inquirer");
+
+//===========================================================================
+// initiate the file system for use with both logging transactions and 
+// reading the random.txt input file
+//===========================================================================
+var fs = require("fs");
 
 //===========================================================================
 // get the users command line arguments which define what we will do
 //===========================================================================
-var fs = require("fs");
-// get the liri command argument
-var liriCmd = process.argv[2];
-// get the artist, song, movie etc argument
-var cmdArg = process.argv[3];
+inquirer
+    .prompt([
+        // What command does the user want to run - give a list to choose from.
+        {
+            type: "list",
+            message: "Which command do you wish to run?",
+            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+            name: "liriCmd"
+        },
+    ])
+    .then(function (inquirerResponse) {
+        //===========================================================================
+        // perform the liri command that the user selected
+        //===========================================================================
+        switch (inquirerResponse.liriCmd) {
 
+            case "movie-this":
+                // search OMDB API for a movie
+                inquirer.prompt([{
+                    type: "input",
+                    message: "what movie?",
+                    name: "movie"
+                },
+                ])
+                    .then(function (inquirerResponse) {
+                        movieSearch(inquirerResponse.movie);
+                    });
+                break;
+            //end "movie-this" search
 
+            case "concert-this":
+                // search Bands in Town Artist Events API
+                inquirer.prompt([{
+                    type: "input",
+                    message: "what band?",
+                    name: "band"
+                },
+                ])
+                    .then(function (inquirerResponse) {
+                        concertSearch(inquirerResponse.band);
+                    });
+                break;
+            //end "concert-this" search
 
+            case "spotify-this-song":
+                // search Spotify API for song information
+                inquirer.prompt([{
+                    type: "input",
+                    message: "what song?",
+                    name: "song"
+                },
+                ])
+                    .then(function (inquirerResponse) {
+                        songSearch(inquirerResponse.song);
+                    });
+                break;
+            //end "spotify-this-song" search
 
+            case "do-what-it-says":
+                fs.readFile("random.txt", "utf8", function (error, data) {
+                    if (error) {
+                        return console.log(error);
+                    } else {
+                        var dataArr = data.split(",");
+                        var liriCmd = dataArr[0];
+                        var cmdArg = dataArr[1];
+                        // execute the command from the random.txt file
+                        switch (liriCmd) {
 
-//===========================================================================
-// run the liri
-//===========================================================================
+                            case "movie-this":
+                                // search OMDB API for a movie
+                                movieSearch(cmdArg);
+                                break;
+                            //end "movie-this" search
 
+                            case "concert-this":
+                                // search Bands in Town Artist Events API
+                                concertSearch(cmdArg);
+                                break;
+                            //end "concert-this" search
 
-//===========================================================================
-// define the use cases
-//===========================================================================
-//  node liri.js
-//  node liri.js concert-this 
-//  node liri.js concert-this "Imagine Dragons"
-//  node liri.js spotify-this-song 
-//  node liri.js spotify-this-song Radioactive
-//  node liri.js movie-this 
-//  node liri.js movie-this Frozen
-//  node liri.js do-what-it-says 
+                            case "spotify-this-song":
+                                // search Spotify API for song information
+                                songSearch(cmdArg);
+                                break;
+                            //end "spotify-this-song" search
 
-//===========================================================================
-// error check and make sure we have two arguments
-//===========================================================================
+                            default:
+                                break;
+                        } // end of switch
+                    }
+                });
+                break;
 
+            default:
+                break;
+        }
 
+        //===========================================================================
+        // log the transaction
+        //===========================================================================
 
+        // do we have log.txt file to log all transactions if not create it and open it
 
+        // create string depicting the transaction 
+        // appendString = "Command:" + liriCmd + " Argument:" + cmdArg;
 
+        // append the transaction to the log.txt file
+        // fs.appendFile("log.txt", appendString, function (err) {
+        //     if (err) { return console.log(err); }
+        // });
 
+    }); //inquirer .then command
 
-//===========================================================================
-// perform the command
-//===========================================================================
-switch (liriCmd) {
+function concertSearch(bandName) {
+    // Create the query we need for BandInTown API with the band specified
+    var queryUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+    // run the BandInTown API query
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(response.data);
+        }
+    )
+    // write event information to the terminal
+    console.log("--------------------------------------------");
+    console.log("Name of Venue: ");
+    console.log("Venue Location: ");
+    console.log("Date of Event: ");
+};
 
-    case "concert-this":
-        // search Bands in Town Artist Events API
-        // create the axios package we need to run query
+function movieSearch(movieName) {
+    // if the movie selection is left blank - default to "Mr Nobody"
+    if (movieName === "") {
+        movieName = "Mr Nobody";
+    }
+    // Create the query we need for OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+    // run the OMDB API query
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(response.data);
+        }
+    )
+    // write movie information to the terminal
+    console.log("--------------------------------------------");
+    console.log("Movie Title: ");
+    console.log("Year movie came out: ");
+    console.log("IMDB Rating: ");
+    console.log("Rotten Tomatoes Rating: ");
+    console.log("Country where movie was produced: ");
+    console.log("Language of movie: ");
+    console.log("Plot of the movie: ");
+    console.log("Actors in the movie: ");
+};
 
-        // write event information to the terminal
-        console.log("Name of Venue: ");
-        console.log("Venue Location: ");
-        console.log("Date of Event: ");
-        break;
-
-    case "spotify-this-song":
-        // search Spotify API for song information
-        // DEFAULT TO ACE OF BASE THE SIGN??????????????????????????????
-        // create the axios package we need to run query
-
-        // write song information to the terminal
-        console.log("Artist(s): ");
-        console.log("Song Name: ");
-        console.log("Preview Link: ");
-        console.log("Album: ");
-        break;
-
-    case "movie-this":
-        // search OMDB API
-        // DEFAULT TO MR NOBODY??????????????????????????????
-        // create the axios package we need to run query
-
-// // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
-// var request = require("request");
-
-// // Then run a request to the OMDB API with the movie specified
-// request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-//   // If the request is successful (i.e. if the response status code is 200)
-//   if (!error && response.statusCode === 200) {
-
-//     // Parse the body of the site and recover just the imdbRating
-//     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-//     console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-//   }
-// });
-
-
-        // write movie information to the terminal
-        console.log("Movie Title: ");
-        console.log("Year movie came out: ");
-        console.log("IMDB Rating: ");
-        console.log("Rotten Tomatoes Rating: ");
-        console.log("Country where movie was produced: ");
-        console.log("Language of movie: ");
-        console.log("Plot of the movie: ");
-        console.log("Actors in the movie: ");
-        break;
-
-    case "do-what-it-says":
-        fs.readFile("random.txt", "utf8", function (error, data) {
-            if (error) { return console.log(error); }
-            var dataArr = data.split(",");
-            liriCmd = dataArr[0];
-            cmdArg = dataArr[1];
-
-        });
-        break;
-
-    default:
-        break;
-}
-
-
-//===========================================================================
-// log the transaction
-//===========================================================================
-
-// do we have log.txt file to log all transactions if not create it and open it
-
-
-// create string depicting the transaction 
-appendString = "Command:" + liriCmd + " Argument:" + cmdArg;
-
-// append the transaction to the log.txt file
-fs.appendFile("log.txt", appendString, function (err) {
-    if (err) { return console.log(err); }
-});
-
-
-
-
-
-
-
-
+function songSearch(songName) {
+    // if the song selection is left blank - default to "The Sign" by "Ace of Base"
+    if (songName === "") {
+        songName = "The Sign";
+        songArtist = "Ace of Base";
+    }
+    // Create the query we need for Spotify API with the song specified
+    var queryUrl = "http://www.spotify...";
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+    // run the spotify API query
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(response.data);
+        }
+    )
+    // write song information to the terminal
+    console.log("--------------------------------------------");
+    console.log("Artist(s): ");
+    console.log("Song Name: ");
+    console.log("Preview Link: ");
+    console.log("Album: ");
+};
